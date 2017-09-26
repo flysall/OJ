36679e2e0b04c7df0823d72cb453f94df191f178
flysall <flysallme@gmail.com>
2017-09-26 14:45:56 +0800

    leetcode;

---
+++ leetcode/lengthOfLastWord.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        if(s.length() == 0)
+            return 0;
+        int start = s.lastIndexOf(" ");
+        if(start == -1)
+            return s.length();
+        if(start == s.length() - 1)
+            return lengthOfLastWord(s.substring(0, s.length() - 1));
+        return s.length() - 1 - start;
+    }
+}
