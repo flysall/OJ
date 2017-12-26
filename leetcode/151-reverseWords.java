669f1e53071525c632d6f934913328e1abcaa6ab
flysall <flysallme@gmail.com>
2017-12-27 07:59:41 +0800

    leetcode: 2017-12-27

--- leetcode/151-reverseWords.java
+++ leetcode/151-reverseWords.java
@@ -1,21 +1,16 @@
-public class Solution {
+class Solution {
     public String reverseWords(String s) {
         String re = "";
         int len = s.length();
         for(int i = 0, j = 0; ; ){
-            if(i == len-1){
-                re = s.substring(j, i+1) + " " + re;
-                break;
-            }
-            while(i < len && s.charAt(i) != ' ')
-                i++;
-            re = s.substring(j, i) + " " + re;
             while(i < len && s.charAt(i) == ' ')
                 i++;
-            if(i == len)
-                break;
+            if(i == len) break;
             j = i;
+            while(i < len && s.charAt(i) != ' ')
+                i++;
+            re = re.length() != 0 ?  s.substring(j, i) + " " + re : s.substring(j, i) + re;
         }
-        return re.trim();
+        return re;
     }
 }
