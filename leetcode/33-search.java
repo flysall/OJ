a23da0e7895698c5f8497c163f4f7c4f5d9731a5
flysall <flysallme@gmail.com>
2017-12-31 22:03:59 +0800

    leetcode: 2017-12-31

--- leetcode/33-search.java
+++ leetcode/33-search.java
@@ -1,21 +1,23 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int len = nums.length;
-        int low = 0, high = len-1;
-        while(low < high){
-            int mid = (low + high) / 2;
-            if(nums[mid] > nums[high]) low = mid+1;
-            else high = mid;
+        if(nums.length == 0 || nums == null) return -1;
+        int start = 0, end = nums.length-1;
+        while(start < end){
+            int mid = (start + end) / 2;
+            if(nums[mid] > nums[end]){
+                if(target > nums[mid] || target <= nums[end])
+                    start = mid+1;
+                else
+                    end = mid;
+            } else{
+                if(target > nums[mid] && target <= nums[end])
+                    start = mid+1;
+                else
+                    end = mid;
+            }
         }
-        int rot = low;
-        low = 0; high = len-1;
-        while(low <= high){
-            int mid = (low + high) / 2;
-            int realmid = (mid + rot) % len;
-            if(nums[realmid] == target) return realmid;
-            if(nums[realmid] < target) low = mid+1;
-            else high = mid-1;
-        }
-        return -1;
+        if(start == end && target != nums[start])
+            return -1;
+        return start;
     }
 }
